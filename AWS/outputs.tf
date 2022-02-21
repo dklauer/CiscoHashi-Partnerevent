6c296fc593d2da65ef3726fce70fea563b9cd716
Cedric Feist <cedric.feist@gmail.com>
2022-02-21 16:38:43 +0100

    Sample

--- AWS/outputs.tf
+++ AWS/outputs.tf
@@ -1,11 +1,11 @@
-output "instance_id" {
+output "instance_id1" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+  value       = aws_instance.app_server[0].id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_instance.app_server[0].public_ip
 }
 
 output "subnet_id" {
